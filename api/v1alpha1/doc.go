ba942af88b92054dcdd9cc3cc2aaac968d4aba2c
wenbing <wenbingincn@gmail.com>
2023-02-08 21:39:38 +0800

    update

---
+++ api/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// +k8s:deepcopy-gen=package
+// +groupName=resource.scheduling.org
+
+package v1alpha1
--- api/v1alpha1/reservation_types.go
+++ api/v1alpha1/reservation_types.go
@@ -20,6 +20,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
 const (
 	ReservationStatusInProgress = "InProgress"
 	ReservationStatusCompleted  = "Completed"
